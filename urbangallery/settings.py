25db0d42e8a0b0fbd6a5d577d564b6b970ea91c4
Glory <nwaforglory6@gmail.com>
2022-06-23 14:18:26 +0100

    Made corrections to the UI as observed by victor

--- urbangallery/settings.py
+++ urbangallery/settings.py
@@ -35,7 +35,7 @@ EMAIL_USE_TLS =True
 EMAIL_BACKEND= 'django.core.mail.backends.smpt.EmailBackend'
 
 
-ALLOWED_HOSTS = ['urbanstores.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
