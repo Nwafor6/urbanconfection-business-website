da60a93f96ddef044a77d8e27a531dd14ba70085
Glory <nwaforglory6@gmail.com>
2022-06-29 04:35:21 +0100

    hid secret key

--- urbangallery/settings.py
+++ urbangallery/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 # SECURITY WARNING: keep the secret key used in production secret!
+import secret
 import django_heroku
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-SECRET_KEY='django-insecure-ydxuy&-w)z^zm&2jg%i5@bg9deya-qd_9xxcn%s-r+8(v-f*qx'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=False
 EMAIL_HOST ='smtp.zoho.com'
